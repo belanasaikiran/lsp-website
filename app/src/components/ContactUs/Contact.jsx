d867097328e2eca86109235ba4a6c1355f1a199b
Saikiran Belana <saikiransanju22@outlook.com>
2022-08-30 10:44:26 +0530

    responsive layout for contact info

--- app/src/components/ContactUs/Contact.jsx
+++ app/src/components/ContactUs/Contact.jsx
@@ -77,8 +77,8 @@ function Contact() {
           </span>
           Us
         </h1>
-        <div className="grid md:grid-cols-2 justify-items-center py-8 ">
-          <div className="md:pl-24">
+        <div className="grid lg:grid-cols-2 justify-items-center py-8 ">
+          <div className="">
             <motion.div
               className="box"
               whileHover={{ scale: 1.05 }}
@@ -86,15 +86,15 @@ function Contact() {
             >
               <iframe
                 title="map"
-                className="rounded-2xl drop-shadow-lg md:h-[600px] md:w-[700px] w-max h-[300px] "
+                className="rounded-2xl drop-shadow-lg xl:h-[600px] xl:w-[700px] lg:h-[500px] lg:w-[650px]   w-screen md:px-10 lg:px-5  h-[300px] "
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63820.57344159325!2d103.80216944525502!3d1.303500827536251!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da1990e8a2fbd3%3A0x9b31156643ec9e88!2s111%20Somerset!5e0!3m2!1sen!2sin!4v1661304390249!5m2!1sen!2sin"
                 loading="lazy"
                 referrerpolicy="no-referrer-when-downgrade"
               ></iframe>
             </motion.div>
           </div>
-          <div className="py-20 md:px-20 px-0">
-            <div className="grid md:grid-cols-2 md:gap-20 md:px-0 px-10 gap-2 text-left align-center justify-evenly   ">
+          <div className="py-20 xl:px-20 lg:px-10 px-0">
+            <div className="grid md:grid-cols-2 lg:gap-20 md:gap-10 md:px-10 px-10 gap-2 text-left align-center justify-evenly   ">
               {/* Contact Info */}
               {ContactInfo.map((info) => (
                 <div>
@@ -109,11 +109,11 @@ function Contact() {
         </div>
       </div>
       {/* write to US */}
-      <div className="py-16 bg-gray-100 md:px-80 ">
+      <div className="py-16 bg-gray-100 xl:px-80 md:px-10 ">
         <h1 className="text-3xl text-red-900 font-bold py-4 ">Write to Us</h1>
         <div className=" py-5    ">
           <form className="space-y-6  " action="#">
-            <div className=" grid md:grid-cols-2 text-gray-900 gap-4 md:px-0 px-8">
+            <div className=" grid xl:grid-cols-2  text-gray-900 gap-4 md:px-0 px-8">
               <input
                 type="text"
                 name="name"
